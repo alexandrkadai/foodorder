dd7f371f11990bba0201ef9c9674440843d7a1e7
Alex Kadai <alexandrkadai@gmail.com>
2022-12-17 12:32:31 +0200

    Is Cart Open

--- src/App.js
+++ src/App.js
@@ -1,16 +1,27 @@
+import { Fragment, useState } from 'react';
 import Header from './components/Layout/Header';
 import Meals from './components/Meals/Meals';
 import Cart from './components/Cart/Cart';
 
 function App() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const showCartHandler = () => {
+    setIsOpen(true);
+  };
+
+  const hideCartHandler = () => {
+    setIsOpen(false);
+  };
+
   return (
-    <>
-      <Cart />
-      <Header />
+    <Fragment>
+      {isOpen && <Cart onCloseCart={hideCartHandler} />}
+      <Header onShowCart={showCartHandler} />
       <main>
         <Meals />
       </main>
-    </>
+    </Fragment>
   );
 }
 
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -11,14 +11,16 @@ const Cart = (props) => {
   );
 
   return (
-    <Modal>
+    <Modal onClick={props.onCloseCart}>
       {cartItems}
       <div className={classes.total}>
         <div>Total Amount</div>
         <div>35.62</div>
       </div>
       <div className={classes.actions}>
-        <button className={classes['button-alt']}>Close</button>
+        <button className={classes['button-alt']} onClick={props.onCloseCart}>
+          Close
+        </button>
         <button className={classes.button}>Order</button>
       </div>
     </Modal>
